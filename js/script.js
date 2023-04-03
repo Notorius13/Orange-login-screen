c6ea7c7683b08e4e90e72273bd07c5cf55b6bf32
Notorius13 <you@example.com>
2023-04-03 14:12:22 +0200

    final version

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var z = document.getElementById("btn");
 function register() {
   x.style.left = "-400px";
   y.style.left = "50px";
-  z.style.left = "115px";
+  z.style.left = "125px";
 }
 function login() {
   x.style.left = "50px";
